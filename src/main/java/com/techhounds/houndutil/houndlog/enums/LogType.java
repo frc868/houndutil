b7502d7b0ee10647fe0d5a6a37270d3e3392c312
superpenguin612 <74030080+superpenguin612@users.noreply.github.com>
2022-09-16 10:06:48 -0400

    Initial commit with updated HoundLog to work in a submodule

---
+++ src/main/java/com/techhounds/houndutil/houndlog/enums/LogType.java
@@ -0,0 +1,16 @@
+package com.techhounds.houndutil.houndlog.enums;
+
+/**
+ * The type of Logger to create. These are values defined by NetworkTables.
+ * 
+ * @author dr
+ */
+public enum LogType {
+    STRING,
+    NUMBER,
+    BOOLEAN,
+    DATA,
+    STRING_ARRAY,
+    NUMBER_ARRAY,
+    BOOLEAN_ARRAY
+}
