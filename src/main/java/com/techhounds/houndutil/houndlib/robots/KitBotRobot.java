ec7e4205f3441ffd2c1785cee7a57ef52b04e699
superpenguin612 <74030080+superpenguin612@users.noreply.github.com>
2023-01-21 12:09:01 -0500

    add kitbotrobot

---
+++ src/main/java/com/techhounds/houndutil/houndlib/robots/KitBotRobot.java
@@ -0,0 +1,28 @@
+package com.techhounds.houndutil.houndlib.robots;
+
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+
+import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+
+public class KitBotRobot extends TimedRobot {
+    private CANSparkMax leftMotor;
+    private CANSparkMax rightMotor;
+    private DifferentialDrive drive;
+    private XboxController controller = new XboxController(0);
+
+    public KitBotRobot(int leftMotorId, int rightMotorId) {
+        leftMotor = new CANSparkMax(leftMotorId, MotorType.kBrushless);
+        rightMotor = new CANSparkMax(leftMotorId, MotorType.kBrushless);
+        rightMotor.setInverted(true);
+        drive = new DifferentialDrive(leftMotor, rightMotor);
+    }
+
+    @Override
+    public void teleopPeriodic() {
+        drive.tankDrive(-controller.getLeftY(), -controller.getRightY());
+    }
+
+}
