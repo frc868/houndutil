f830066fbdcfa11a8fb1e4361add5b88243e1471
superpenguin612 <74030080+superpenguin612@users.noreply.github.com>
2023-01-26 15:43:51 -0500

    add limitMechanism helper

---
+++ src/main/java/com/techhounds/houndutil/houndlib/Utils.java
@@ -0,0 +1,8 @@
+package com.techhounds.houndutil.houndlib;
+
+public class Utils {
+
+    public static boolean limitMechanism(boolean isLowerTriggered, boolean isUpperTriggered, double value) {
+        return (value > 0.0 && isUpperTriggered) || (value < 0.0 && isLowerTriggered);
+    }
+}
