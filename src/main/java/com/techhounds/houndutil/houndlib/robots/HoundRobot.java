14ab2a3742f2ec7557dc30b4f22e7d74e3e3f0c9
superpenguin612 <74030080+superpenguin612@users.noreply.github.com>
2023-03-03 09:52:48 -0500

    make pose estimator run at 0.1s period instead of 0.5

--- src/main/java/com/techhounds/houndutil/houndlib/robots/HoundRobot.java
+++ src/main/java/com/techhounds/houndutil/houndlib/robots/HoundRobot.java
@@ -26,7 +26,7 @@ public class HoundRobot extends TimedRobot {
         // main thread
         LoggingManager.getInstance().init();
         addPeriodic(LoggingManager.getInstance()::run, 0.1, 0.010);
-        addPeriodic(AutoManager.getInstance()::updatePoseEstimator, 0.5, 0.010);
+        addPeriodic(AutoManager.getInstance()::updatePoseEstimator, 0.1, 0.010);
 
         LiveWindow.disableAllTelemetry();
     }
