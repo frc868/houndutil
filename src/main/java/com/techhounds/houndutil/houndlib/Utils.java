1add9da8cf39da3d534e8d4a23bc6dc6c722596c
superpenguin612 <74030080+superpenguin612@users.noreply.github.com>
2023-12-20 20:15:30 -0500

    Add MotorHoldMode

--- src/main/java/com/techhounds/houndutil/houndlib/Utils.java
+++ src/main/java/com/techhounds/houndutil/houndlib/Utils.java
@@ -1,7 +1,6 @@
 package com.techhounds.houndutil.houndlib;
 
 public class Utils {
-
     public static boolean limitMechanism(boolean isLowerTriggered, boolean isUpperTriggered, double value) {
         return (value > 0.0 && isUpperTriggered) || (value < 0.0 && isLowerTriggered);
     }
