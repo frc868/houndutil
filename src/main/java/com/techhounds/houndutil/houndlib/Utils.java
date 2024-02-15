6d1faa57e0b3934ccbf9d2803863f7d295374a89
superpenguin612 <74030080+superpenguin612@users.noreply.github.com>
2024-02-15 15:46:31 -0500

    Add `applySoftStops`

--- src/main/java/com/techhounds/houndutil/houndlib/Utils.java
+++ src/main/java/com/techhounds/houndutil/houndlib/Utils.java
@@ -4,4 +4,8 @@ public class Utils {
     public static boolean limitMechanism(boolean isLowerTriggered, boolean isUpperTriggered, double value) {
         return (value > 0.0 && isUpperTriggered) || (value < 0.0 && isLowerTriggered);
     }
+
+    public static double applySoftStops(double voltage, double position, double minPosition, double maxPosition) {
+        return (voltage > 0.0 && position >= maxPosition) || (voltage < 0.0 && position <= minPosition) ? 0 : voltage;
+    }
 }
