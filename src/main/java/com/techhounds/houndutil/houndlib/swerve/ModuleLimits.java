5ebb57b8ebb96dfb4c55fc0fa550ddbf446a7731
superpenguin612 <74030080+superpenguin612@users.noreply.github.com>
2024-02-25 08:43:42 -0500

    Add 254 swerve setpoint generator

---
+++ src/main/java/com/techhounds/houndutil/houndlib/swerve/ModuleLimits.java
@@ -0,0 +1,5 @@
+package com.techhounds.houndutil.houndlib.swerve;
+
+public record ModuleLimits(
+        double maxDriveVelocity, double maxDriveAcceleration, double maxSteeringVelocity) {
+}
---
+++ src/main/java/com/techhounds/houndutil/houndlib/swerve/SwerveSetpoint.java
@@ -0,0 +1,7 @@
+package com.techhounds.houndutil.houndlib.swerve;
+
+import edu.wpi.first.math.kinematics.ChassisSpeeds;
+import edu.wpi.first.math.kinematics.SwerveModuleState;
+
+public record SwerveSetpoint(ChassisSpeeds chassisSpeeds, SwerveModuleState[] moduleStates) {
+}
