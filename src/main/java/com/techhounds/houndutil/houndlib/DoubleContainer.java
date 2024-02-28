e6564f1a275175abbf34b879c3d97169d583372e
superpenguin612 <74030080+superpenguin612@users.noreply.github.com>
2024-02-28 16:51:39 -0500

    Add doublecontainer

---
+++ src/main/java/com/techhounds/houndutil/houndlib/DoubleContainer.java
@@ -0,0 +1,9 @@
+package com.techhounds.houndutil.houndlib;
+
+public class DoubleContainer {
+    public double value;
+
+    public DoubleContainer(double value) {
+        this.value = value;
+    }
+}
