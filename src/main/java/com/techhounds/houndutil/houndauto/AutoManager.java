1ee38451d1e838a1499f0d13cca8d9fd2a9b4b8b
superpenguin612 <74030080+superpenguin612@users.noreply.github.com>
2024-03-19 15:43:47 -0400

    Remove unused import

--- src/main/java/com/techhounds/houndutil/houndauto/AutoManager.java
+++ src/main/java/com/techhounds/houndutil/houndauto/AutoManager.java
@@ -23,7 +23,6 @@ import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 import edu.wpi.first.wpilibj2.command.Commands;
-import edu.wpi.first.wpilibj2.command.button.Trigger;
 
 public class AutoManager {
     private static AutoManager instance;
