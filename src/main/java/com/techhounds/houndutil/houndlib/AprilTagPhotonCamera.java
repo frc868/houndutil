4a636546b32a58c6e43a3a6a1117c426161953fd
David Racovan <davidracovan@yahoo.com>
2025-03-05 10:38:10 -0500

    Remove unused imports

--- src/main/java/com/techhounds/houndutil/houndlib/AprilTagPhotonCamera.java
+++ src/main/java/com/techhounds/houndutil/houndlib/AprilTagPhotonCamera.java
@@ -6,7 +6,6 @@ import java.util.Optional;
 import org.photonvision.EstimatedRobotPose;
 import org.photonvision.PhotonCamera;
 import org.photonvision.PhotonPoseEstimator;
-import org.photonvision.PhotonPoseEstimator.ConstrainedSolvepnpParams;
 import org.photonvision.PhotonPoseEstimator.PoseStrategy;
 import org.photonvision.simulation.PhotonCameraSim;
 import org.photonvision.simulation.SimCameraProperties;
