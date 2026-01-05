5e9ace806c178cee5b566adf555fce31c285d766
David Racovan <davidracovan@yahoo.com>
2026-01-05 16:21:35 -0500

    Add utils from 2025

--- src/main/java/com/techhounds/houndutil/houndlib/Utils.java
+++ src/main/java/com/techhounds/houndutil/houndlib/Utils.java
@@ -1,7 +1,12 @@
 package com.techhounds.houndutil.houndlib;
 
+import com.techhounds.houndutil.houndauto.Reflector;
+
+import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Twist2d;
 import edu.wpi.first.math.kinematics.ChassisSpeeds;
+import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj.DriverStation.Alliance;
 
 /**
  * A collection of static utility functions used throughout the library, which
@@ -96,5 +101,39 @@ public class Utils {
      */
     public static int interpolate(int start, int end, double t) {
         return (int) (start + (end - start) * t);
+    }
+
+    public static double getLineDistance(Pose2d pose, Pose2d lineApexPose) {
+        double cosTheta = lineApexPose.getRotation().getCos();
+        double sinTheta = lineApexPose.getRotation().getSin();
+
+        double A = -sinTheta;
+        double B = cosTheta;
+        double C = -(A * lineApexPose.getX() + B * lineApexPose.getY());
+
+        // formula for distance between point and line given Ax + By + C = 0
+        double distance = (A * pose.getX() + B * pose.getY() + C)
+                / Math.sqrt(A * A + B * B);
+        return distance;
+    }
+
+    public static Pose2d getClosestPoseOnLine(Pose2d pose, Pose2d lineApexPose) {
+        double cosTheta = lineApexPose.getRotation().getCos();
+        double sinTheta = lineApexPose.getRotation().getSin();
+
+        double A = -sinTheta;
+        double B = cosTheta;
+        double C = -(A * lineApexPose.getX() + B * lineApexPose.getY());
+
+        // Project the point onto the line using the formula for closest point
+        // projection
+        double xClosest = (B * (B * pose.getX() - A * pose.getY()) - A * C) / (A * A + B * B);
+        double yClosest = (A * (-B * pose.getX() + A * pose.getY()) - B * C) / (A * A + B * B);
+
+        return new Pose2d(xClosest, yClosest, lineApexPose.getRotation());
+    }
+
+    public static boolean shouldFlipValueToRed() {
+        return DriverStation.getAlliance().isPresent() && DriverStation.getAlliance().get() == Alliance.Red;
     }
 }
