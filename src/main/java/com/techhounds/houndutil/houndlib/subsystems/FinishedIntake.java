c8562106a2a473a2b1af60b316f308e7cecfe963
EeriestBus <elliottws888@gmail.com>
2026-06-25 14:16:33 -0400

    add intake

---
+++ src/main/java/com/techhounds/houndutil/houndlib/subsystems/FinishedIntake.java
@@ -0,0 +1,31 @@
+package com.techhounds.houndutil.houndlib.subsystems;
+
+import edu.wpi.first.wpilibj2.command.Command;
+
+/**
+ * Base scaffolding for an intake mechanism.
+ */
+public interface FinishedIntake {
+    /**
+     * Creates a command that runs the rollers of the intake in the direction that
+     * will index the object (likely a game piece) into the robot.
+     * 
+     * @return the command
+     */
+    public Command runRollersCommand();
+
+    /**
+     * Creates a command that runs the rollers of the intake in the direction that
+     * will eject the object (likely a game piece) out of the robot.
+     * 
+     * @return the command
+     */
+    public Command reverseRollersCommand();
+
+    /**
+     * Creates a command that stops the rollers of the intake.
+     * 
+     * @return the command
+     */
+    public Command stopRollersCommand();
+}
