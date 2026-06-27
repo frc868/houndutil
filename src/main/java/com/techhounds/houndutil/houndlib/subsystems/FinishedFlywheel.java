822249b23c03d3f96912a79f11a894aaabba9646
EeriestBus <elliottws888@gmail.com>
2026-06-27 16:35:41 -0400

    finish draft finishedFlywheel

--- src/main/java/com/techhounds/houndutil/houndlib/subsystems/FinishedFlywheel.java
+++ src/main/java/com/techhounds/houndutil/houndlib/subsystems/FinishedFlywheel.java
@@ -2,9 +2,12 @@ package com.techhounds.houndutil.houndlib.subsystems;
 
 import static edu.wpi.first.units.Units.*;
 
+import java.util.function.Supplier;
+
 import com.ctre.phoenix6.CANBus;
 import com.ctre.phoenix6.configs.TalonFXConfiguration;
 import com.ctre.phoenix6.controls.ControlRequest;
+import com.ctre.phoenix6.controls.MotionMagicVelocityVoltage;
 import com.ctre.phoenix6.controls.StrictFollower;
 import com.ctre.phoenix6.controls.VoltageOut;
 import com.ctre.phoenix6.hardware.TalonFX;
@@ -14,14 +17,17 @@ import com.techhounds.houndutil.houndlog.LogProfiles;
 import com.techhounds.houndutil.houndlog.LoggingManager;
 import com.techhounds.houndutil.houndlog.loggers.LogGroup;
 
+import edu.wpi.first.math.MathUtil;
 import edu.wpi.first.math.system.plant.DCMotor;
 import edu.wpi.first.math.system.plant.LinearSystemId;
+import edu.wpi.first.units.measure.AngularVelocity;
 import edu.wpi.first.units.measure.Current;
 import edu.wpi.first.units.measure.MomentOfInertia;
 import edu.wpi.first.units.measure.Voltage;
 import edu.wpi.first.wpilibj.RobotBase;
 import edu.wpi.first.wpilibj.simulation.FlywheelSim;
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Command.InterruptionBehavior;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 //NOT a logged object (kinda)
@@ -41,48 +47,91 @@ public class FinishedFlywheel extends SubsystemBase{
     public final DCMotor MOTOR_GEARBOX_REPR;
     public final MomentOfInertia MOMENT_OF_INERTIA;
     public final CANBus CANBUS;
+    public final double[] K;
 
     public final TalonFXConfiguration config = new TalonFXConfiguration();
     public final TalonFX[] motors;
     public final FlywheelSim[] sim;
     public final StrictFollower followerRequest;
     public final VoltageOut voltageRequest = new VoltageOut(0.0).withEnableFOC(true).withUseTimesync(true);
+    private final MotionMagicVelocityVoltage velocityRequest = new MotionMagicVelocityVoltage(RotationsPerSecond.zero()).withEnableFOC(true).withUseTimesync(true);
+    public AngularVelocity goalVelocity = RotationsPerSecond.zero();
 
     /**
-     * Creates a command that runs the rollers of the intake in the direction that
-     * will index the object (likely a game piece) into the robot.
+     * Gets the velocity of the flywheel. 0 should indicate it being stopped, and
+     * the velocity should increase in the forward direction (i.e. the velocity
+     * should be positive in the "correct" direction).
      * 
-     * @return the command
+     * @return the velocity of the flywheel
      */
-    public Command runRollersCommand(){
-        return runEnd(
-            () -> setMotorsControl(voltageRequest.withOutput(INTAKE_VOLTAGE)),
-            () -> stop()
-        ).withName(NAME + ".runRollers");
+    public AngularVelocity getVelocity(){
+        AngularVelocity total = RotationsPerSecond.zero();
+        int i = 0;
+        for(TalonFX motor: motors){
+            total = total.plus(motor.getVelocity().getValue().times(TALON_INFO[i].INVERT == InvertedValue.Clockwise_Positive ? 1 : -1));
+            i ++;
+        }
+        total = total.div(i);
+
+        return total;
     }
 
     /**
-     * Creates a command that runs the rollers of the intake in the direction that
-     * will eject the object (likely a game piece) out of the robot.
+     * Explicit function to set the voltage of the motors attached to the elevator,
+     * should handle safeties and clamping here.
      * 
-     * @return the command
+     * @param voltage the voltage to apply to the motors, [-12, 12]
      */
-    public Command reverseRollersCommand(){
-        return runEnd(
-            () -> setMotorsControl(voltageRequest.withOutput(REVERSE_VOLTAGE)),
-            () -> stop()
-        ).withName(NAME + ".reverseRollers");
+    public void setVoltage(Voltage voltage){
+        setMotorsControl(voltageRequest.withOutput(MathUtil.clamp(voltage.in(Volts) , - 12 , 12)));
     }
 
     /**
-     * Creates a command that stops the rollers of the intake.
+     * Creates a command that continuously spins the flywheel at a specific velocity
+     * until cancelled. Note that this is *not* intended to self-cancel after
+     * reaching its setpoint and defer to a default command.
      * 
+     * @param goalVelocitySupplier a supplier of a velocity to spin at
      * @return the command
      */
-    public Command stopRollersCommand(){
-        return runOnce(
-            () -> stop()
-        ).withName(NAME + ".stopRollers");
+    public Command spinAtVelocityCommand(Supplier<AngularVelocity> goalVelocitySupplier){
+        return runEnd(() -> {
+            goalVelocity = goalVelocitySupplier.get();
+            setMotorsControl(velocityRequest.withVelocity(goalVelocity));
+        }, () -> stop()).withName(NAME + ".spinAtVelocity");
+
+    }
+
+    /**
+     * Creates a command that manually sets the speed of the mechanism. Useful for
+     * overriding PID control.
+     * 
+     * @apiNote use {@code setVoltage()} internally in order to maintain safeties
+     *          and clamping
+     * 
+     * @param speed the speed [-1,1]
+     * @return
+     */
+    public Command setOverridenSpeedCommand(Supplier<Double> speed){
+        return runEnd(
+            () -> setVoltage(Volts.of(speed.get() * 12)),
+            () -> stop()).withName(NAME + ".setOverridenSpeedCommand");
+    }
+
+    /**
+     * Creates a command stops the motor and sets it to coast mode, to allow for
+     * moving the mechanism manually.
+     * 
+     * @apiNote use
+     *          {@code .withInterruptBehavior(InterruptionBehavior.kCancelIncoming)}
+     *          for safety
+     * @return the command
+     */
+    public Command coastMotorsCommand(){
+        return startEnd(
+            () -> {stop(); for(TalonFX motor: motors){motor.setNeutralMode(NeutralModeValue.Coast);}}, 
+            () -> {        for(TalonFX motor: motors){motor.setNeutralMode(NEUTRAL);} goalVelocity = getVelocity();}
+            ).withInterruptBehavior(InterruptionBehavior.kCancelIncoming).withName(NAME + ".coastMotors");
     }
 
     @Override
@@ -113,7 +162,7 @@ public class FinishedFlywheel extends SubsystemBase{
     }
     
 
-    public FinishedFlywheel(FinishedTalonSystem[] talonInfo, boolean areFollowers, String name, Voltage intakeVoltage, Voltage reverseVoltage, Current currentLimit, double gearRatio, NeutralModeValue neutral, DCMotor motorGearboxRepr, MomentOfInertia momentOfInertia, CANBus bus){
+    public FinishedFlywheel(FinishedTalonSystem[] talonInfo, boolean areFollowers, String name, Voltage intakeVoltage, Voltage reverseVoltage, Current currentLimit, double gearRatio, NeutralModeValue neutral, DCMotor motorGearboxRepr, MomentOfInertia momentOfInertia, CANBus bus, double[] tuningConstants){
         TALON_INFO = talonInfo;
         ARE_FOLLOWERS = areFollowers;
         NAME = name;
@@ -125,6 +174,7 @@ public class FinishedFlywheel extends SubsystemBase{
         MOTOR_GEARBOX_REPR = motorGearboxRepr;
         MOMENT_OF_INERTIA = momentOfInertia;
         CANBUS = bus;
+        K = tuningConstants;
 
         followerRequest = new StrictFollower(TALON_INFO[0].CAN_ID);
         motors = new TalonFX[TALON_INFO.length];
@@ -156,6 +206,7 @@ public class FinishedFlywheel extends SubsystemBase{
         for(int i = 0; i < motors.length; i++){
             motors[i] = new TalonFX(TALON_INFO[i].CAN_ID, CANBUS);
             config.MotorOutput.Inverted = TALON_INFO[i].INVERT;
+            config.Slot0.withKA(K[0]).withKD(K[1]).withKG(K[2]).withKI(K[3]).withKP(K[4]).withKS(K[5]).withKV(K[6]);
             motors[i].getConfigurator().apply(config);
 
             if(i != 0 && ARE_FOLLOWERS){
