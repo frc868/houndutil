ed352906967eb9579b254dcd06e8c5a23c6e6665
Vedanth <vedanthkrishna0820@gmail.com>
2026-06-30 17:52:22 -0400

    create folder for finished subsystems like in elliott's branch

--- src/main/java/com/techhounds/houndutil/houndlib/subsystems/finished/FinishedFlywheel.java
+++ src/main/java/com/techhounds/houndutil/houndlib/subsystems/finished/FinishedFlywheel.java
@@ -1,4 +1,4 @@
-package com.techhounds.houndutil.houndlib.subsystems;
+package com.techhounds.houndutil.houndlib.subsystems.finished;
 
 import static edu.wpi.first.units.Units.*;
 
@@ -13,6 +13,7 @@ import com.ctre.phoenix6.controls.VoltageOut;
 import com.ctre.phoenix6.hardware.TalonFX;
 import com.ctre.phoenix6.signals.InvertedValue;
 import com.ctre.phoenix6.signals.NeutralModeValue;
+import com.techhounds.houndutil.houndlib.subsystems.FinishedTalonSystem;
 import com.techhounds.houndutil.houndlog.LogProfiles;
 import com.techhounds.houndutil.houndlog.LoggingManager;
 import com.techhounds.houndutil.houndlog.loggers.LogGroup;
--- src/main/java/com/techhounds/houndutil/houndlib/subsystems/finished/FinishedSuperstructure.java
+++ src/main/java/com/techhounds/houndutil/houndlib/subsystems/finished/FinishedSuperstructure.java
@@ -1,4 +1,4 @@
-package com.techhounds.houndutil.houndlib.subsystems;
+package com.techhounds.houndutil.houndlib.subsystems.finished;
 
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
