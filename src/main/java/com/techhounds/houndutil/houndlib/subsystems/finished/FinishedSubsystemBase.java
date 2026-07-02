70091c14ca9caa5021004bb9777a39e3d0cececa
EeriestBus <elliottws888@gmail.com>
2026-07-02 12:37:51 -0400

    (#1) move KrakenType into it's own folder

--- src/main/java/com/techhounds/houndutil/houndlib/subsystems/finished/FinishedSubsystemBase.java
+++ src/main/java/com/techhounds/houndutil/houndlib/subsystems/finished/FinishedSubsystemBase.java
@@ -3,18 +3,4 @@ package com.techhounds.houndutil.houndlib.subsystems.finished;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class FinishedSubsystemBase extends SubsystemBase{
-    public enum KrakenType{
-        SIXTY(60),
-        FORTY_FOUR(44);
-
-        private KrakenType(int n){
-            this.n = n;
-        }
-
-        private int n;
-
-        public int getInt() {
-            return n;
-        }
-    }
 }
