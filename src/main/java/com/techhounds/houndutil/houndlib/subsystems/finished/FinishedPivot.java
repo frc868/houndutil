d1cf7052752887f3f69d0645fa1e79d996ef3655
EeriestBus <elliottws888@gmail.com>
2026-08-08 10:27:18 -0400

    remove unused import

--- src/main/java/com/techhounds/houndutil/houndlib/subsystems/finished/FinishedPivot.java
+++ src/main/java/com/techhounds/houndutil/houndlib/subsystems/finished/FinishedPivot.java
@@ -13,7 +13,6 @@ import com.ctre.phoenix6.controls.StrictFollower;
 import com.ctre.phoenix6.controls.VoltageOut;
 import com.ctre.phoenix6.hardware.TalonFX;
 import com.ctre.phoenix6.signals.GravityTypeValue;
-import com.ctre.phoenix6.signals.InvertedValue;
 import com.ctre.phoenix6.signals.NeutralModeValue;
 import com.techhounds.houndutil.houndlib.TuningConstants;
 import com.techhounds.houndutil.houndlib.Utils;
